dc05325f8242f2783f99a85d5a69a76c71f1d896
hyoomi <80243578+hyoomi@users.noreply.github.com>
2022-05-02 21:06:00 +0900

    Add Folders

---
+++ Assets/Temp/효민/TesMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesMove : MonoBehaviour
+{
+
+    public float moveSpeed = 10f;
+    Vector2 moveVec;
+
+    //[SerializeField]
+    Rigidbody2D rigid;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //moveVec = Vector2.zero;
+    }
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        moveVec = Vector2.zero;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            moveVec.x = -moveSpeed;
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            moveVec.x = moveSpeed;
+
+        rigid.velocity = moveVec;
+    }
+}
---
+++ Assets/Temp/효민/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public GameObject go;
+
+    public void SetTure()
+    {
+        go.SetActive(true);
+    }
+    public void SetFalse()
+    {
+        go.SetActive(false);
+    }
+}
