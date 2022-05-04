592409aa95f2bd110dfd462e2b9d7dec2eeac0b7
Wjadebead <wkiju49@gmail.com>
2022-05-04 12:48:31 +0900

    Add option
    오류나서 이름 폴더 영문으로 바꿨습니다.

---
+++ Assets/Temp/Kiju/Option.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Option : MonoBehaviour
+{
+    private bool toggle = false;
+    public GameObject panel;
+    public AudioMixer master;
+    public Slider slider;
+    
+    public void Panel_Set(){
+        toggle = !toggle;
+        panel.SetActive(toggle);
+    }
+
+    public void Sound_Control(){
+        float volume = slider.value;
+        if(volume == -40f){
+            master.SetFloat("Master", -80);
+        }
+        else{
+            master.SetFloat("Master", volume);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+}
