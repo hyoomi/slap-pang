e2de197435a567ec8074449a82dc791a942fba23
penguiin1 <104720787+penguiin1@users.noreply.github.com>
2022-05-07 00:40:07 +0900

    Timer

---
+++ Assets/Temp/Junhyuk/TimerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    Slider Timer;
+    public float MaxTime = 8f;
+    float timeLeft;
+    public GameObject Gameover;
+    void Start()
+    {
+        Gameover.SetActive(false);
+        Timer = GetComponent<Slider>();
+        timeLeft = MaxTime;
+    }
+
+
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            Timer.value = timeLeft / MaxTime;
+        }
+        else
+        {
+            Gameover.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
