a9774d8672bde5a32f6a1a5af2fd7b9d1e6fa021
naro-Kim <gracias9022@gmail.com>
2022-05-07 12:25:13 +0900

    Add GameStart( ), PauseBtnEvent( )
    1. 코루틴을 이용해 게임 시작 2초후 "Game!"에서 "Start"로 문구가 바뀌도록 하였습니다.
    
    2. Pause Button UI를 통해 팝업을 열고 닫도록 만들었습니다.

--- Assets/Temp/Ahhyun/GameStart_3.cs
+++ Assets/Temp/Ahhyun/GameStart_3.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /*
 - <게임 시작3>에서 [GAME!] 문구가 뜬 후 2초뒤 [START]로 바꿔주기 (코루틴 사용)
@@ -9,18 +10,19 @@ using UnityEngine;
 
 public class GameStart_3 : MonoBehaviour
 {
+    [SerializeField] Text GameStartText;
     // Start is called before the first frame update
     void Start()
     {
-        print("Game!");
-        StartCoroutine(StartPrint);
+        StartCoroutine(StartPrint());
     }
 
     IEnumerator StartPrint()
     {
         yield return new WaitForSeconds(2.0f);
+        GameStartText.text = "START";
         // Change UI into Start Pop up 
-        Debug.Log("executed coroutine")
+        Debug.Log("executed coroutine");
     }
 
     // Update is called once per frame
--- Assets/Temp/Ahhyun/PauseButtonEvent.cs
+++ Assets/Temp/Ahhyun/PauseButtonEvent.cs
@@ -2,23 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+ 
 public class PauseButtonEvent : MonoBehaviour
-{
+{ 
+    //일시멈춤 버튼의 텍스트와 Bool값
+    [SerializeField] Text startPauseText;
     bool isPauseActive = false;
+
+    //Popup을 띄우기 위한 패널
+    public GameObject Panel;
 
     public void pauseBtn()
     {
-        if(isPauseActive)
+        if (Panel != null) // 팝업을 띄우기 위한 if문
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+        if (isPauseActive)
         {
             Time.timeScale = 1;
-            isPauseActive = false;
+            isPauseActive = false; 
+            Debug.Log("Resume"); 
         }
         else
         {
             Time.timeScale = 0;
             isPauseActive = true;
+            Debug.Log("Paused"); 
         }
         // inner text change
-    }
+        startPauseText.text = isPauseActive ? "RESUME" : "PAUSE";
+    }   
 }
