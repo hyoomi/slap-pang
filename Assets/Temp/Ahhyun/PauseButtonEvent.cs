d6e360ed6f9630e3d459417afb7cf8471e1ee283
naro-Kim <gracias9022@gmail.com>
2022-05-12 10:43:35 +0900

    Update PauseButtonEvent.cs
    3번 사항을 수정했습니다.

--- Assets/Temp/Ahhyun/PauseButtonEvent.cs
+++ Assets/Temp/Ahhyun/PauseButtonEvent.cs
@@ -3,35 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
  
-public class PauseButtonEvent : MonoBehaviour
-{ 
+public class PauseButtonEvent : BaseUI
+{
     //일시멈춤 버튼의 텍스트와 Bool값
     [SerializeField] Text startPauseText;
-    bool isPauseActive = false;
 
-    //Popup을 띄우기 위한 패널
-    public GameObject Panel;
 
-    public void pauseBtn()
+    public void PauseBtn(GameObject go)
     {
-        if (Panel != null) // 팝업을 띄우기 위한 if문
+        bool IsPauseActive = go.activeSelf;
+        if (go != null) // 팝업을 띄우기 위한 if문
         {
-            bool isActive = Panel.activeSelf;
-            Panel.SetActive(!isActive);
+            SetTrue(go);
         }
-        if (isPauseActive)
+        if (IsPauseActive)
         {
             Time.timeScale = 1;
-            isPauseActive = false; 
-            Debug.Log("Resume"); 
+            SetFalse(go);
         }
         else
         {
             Time.timeScale = 0;
-            isPauseActive = true;
-            Debug.Log("Paused"); 
+            SetTrue(go);
         }
         // inner text change
-        startPauseText.text = isPauseActive ? "RESUME" : "PAUSE";
+        startPauseText.text = IsPauseActive ? "FAUSE" : "RESUME";
     }   
 }
