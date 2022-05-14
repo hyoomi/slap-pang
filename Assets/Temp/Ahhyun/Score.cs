77a7762e61c208eb267fe86ad1fc53d88fd06934
naro_Kim <gracias9022@gmail.com>
2022-05-14 22:06:18 +0900

    Add Score.cs
    Score를 쉼표와 함께 리턴하는 Score.cs와 Score.prefab을 Temp/Ahhyun에 넣어두었습니다.

---
+++ Assets/Temp/Ahhyun/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] Text scorePrefab;
+    //public Text ScoreText;
+
+    public string GetCommaScore(ulong data)
+    {
+        //uint 최댓값 자릿수만큼 정규식으로 표현한 점수.. 
+        return string.Format("{0:#,###,###,###}",data);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ScoreText.text = Managers.data.score.ToString();
+        //쉼표 출력을 위해 GetCommaScore 함수를 사용했습니다
+        scorePrefab.text = GetCommaScore(Managers.Data.score).ToString();
+    }
+}
