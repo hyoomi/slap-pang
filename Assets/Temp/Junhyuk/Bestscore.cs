1b8512d0b89cc15dff660a3cf0bdf664c47b7d4e
penguiin1 <104720787+penguiin1@users.noreply.github.com>
2022-05-22 05:10:06 +0900

    BS createxml

--- Assets/Temp/Junhyuk/Bestscore.cs
+++ Assets/Temp/Junhyuk/Bestscore.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
+using UnityEngine.UI;
 
 public class Bestscore : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        CreateXml();
     }
 
-    // Update is called once per frame
+    void CreateXml()
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.AppendChild(xmlDoc.CreateXmlDeclaration("1.0", "utf-8", "yes"));
+
+        XmlNode root = xmlDoc.CreateNode(XmlNodeType.Element, "Bestscore", string.Empty);
+        xmlDoc.AppendChild(root);
+
+        XmlNode child = xmlDoc.CreateNode(XmlNodeType.Element, "Bestscore", string.Empty);
+        root.AppendChild(child);
+
+        XmlElement Score = xmlDoc.CreateElement("Score");
+        Score.InnerText = "d";
+        child.AppendChild(Score);
+
+    }
+
     void Update()
     {
         
