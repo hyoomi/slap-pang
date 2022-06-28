94d6be3486793671f4428965de037ce9c9ef31f2
hyoomi <80243578+hyoomi@users.noreply.github.com>
2022-06-29 01:18:18 +0900

    Add Tutorial

---
+++ Assets/Scripts/UI/Popup/Tutorial.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : PopupUI
+{
+    [SerializeField]
+    List<GameObject> TutorialPopup;
+    int page;
+
+    private void Start()
+    {
+        page = 1;
+
+        foreach (var tut in TutorialPopup){
+            tut.SetActive(false);
+        }
+
+        TutorialPopup[0].SetActive(true);
+    }
+
+    public void NextTutorial()
+    {
+        if(page >= TutorialPopup.Count)
+        {
+            ClosePopup();
+            return;
+        }
+
+        TutorialPopup[page].SetActive(true);
+        page++;
+    }
+}
